46364177fa082767b7e84979899f0b41bc60a5ff
budurmete <budurmete@gmail.com>
2022-09-22 14:15:52 +0300

    Proje dosyası ekle.

---
+++ At_Yarisi/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace At_Yarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblBitisCizgisi = new System.Windows.Forms.Label();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBitisCizgisi
+            // 
+            this.lblBitisCizgisi.BackColor = System.Drawing.Color.Transparent;
+            this.lblBitisCizgisi.Location = new System.Drawing.Point(1087, 1);
+            this.lblBitisCizgisi.Name = "lblBitisCizgisi";
+            this.lblBitisCizgisi.Size = new System.Drawing.Size(58, 562);
+            this.lblBitisCizgisi.TabIndex = 5;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.BackColor = System.Drawing.Color.Red;
+            this.btnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslat.Location = new System.Drawing.Point(1030, 18);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(104, 70);
+            this.btnBaslat.TabIndex = 6;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = false;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::At_Yarisi.Properties.Resources.hrs3;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 454);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 109);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.BackColor = System.Drawing.Color.Transparent;
+            this.lblsonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsonuc.Location = new System.Drawing.Point(339, 18);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(437, 58);
+            this.lblsonuc.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::At_Yarisi.Properties.Resources.at_gif1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 194);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::At_Yarisi.Properties.Resources.AT_YARIŞI2;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 323);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 125);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chartreuse;
+            this.BackgroundImage = global::At_Yarisi.Properties.Resources._2d_at_yarisiARKAPLAN;
+            this.ClientSize = new System.Drawing.Size(1146, 602);
+            this.Controls.Add(this.lblsonuc);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.lblBitisCizgisi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblBitisCizgisi;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblsonuc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ At_Yarisi/Form1.cs
@@ -0,0 +1,97 @@
+﻿using SpeechLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace At_Yarisi
+{
+    public partial class Form1 : Form
+    {
+        int BirinciAtSolaUzaklik, ikinciAtSolaUzaklik, UcuncuAtSolaUzaklik;
+        int bittimi = 0;
+        Random rastgele = new Random();
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SpVoice ses = new SpVoice();
+            SpeechRecognitionEngine sr = new SpeechRecognitionEngine();
+            Grammar g = new DictationGrammar();
+            sr.LoadGrammar(g);
+            int birinciatinGenisligi = pictureBox1.Width;
+            int ikinciatinGenisligi = pictureBox2.Width;
+            int ucuncuatinGenisligi = pictureBox3.Width;
+            int BitisUzakligi = lblBitisCizgisi.Left;
+            pictureBox1.Left = pictureBox1.Left + rastgele.Next(5,15);
+            pictureBox2.Left = pictureBox2.Left + rastgele.Next(5,15);
+            pictureBox3.Left = pictureBox3.Left + rastgele.Next(5,15);
+
+            if (pictureBox1.Left>pictureBox2.Left+5&&pictureBox2.Left>pictureBox3.Left+5)
+            {
+                lblsonuc.Text = "1 Numaralı At önde ";
+            }
+            if (pictureBox2.Left > pictureBox1.Left + 5 && pictureBox2.Left > pictureBox3.Left + 5)
+            {
+                lblsonuc.Text = "2 Numaralı At önde ";
+            }
+            if (pictureBox3.Left > pictureBox1.Left + 5 && pictureBox3.Left > pictureBox2.Left + 5)
+            {
+                lblsonuc.Text = "3 Numaralı At önde ";
+            }
+
+
+            if (birinciatinGenisligi+pictureBox1.Left>=BitisUzakligi)
+            {
+                bittimi = 1;
+                timer1.Enabled = false;
+                ses.Speak("1 numaralı at yarışı kazandı");
+            }
+
+            if (ikinciatinGenisligi + pictureBox2.Left >= BitisUzakligi)
+            {
+                bittimi = 1;
+                timer1.Enabled = false;
+                ses.Speak("2 numaralı at yarışı kazandı");
+            }
+
+            if (ucuncuatinGenisligi + pictureBox3.Left >= BitisUzakligi)
+            {
+                bittimi = 1;
+                timer1.Enabled = false;
+                ses.Speak("3 numaralı at yarışı kazandı");
+            }
+
+            if (bittimi == 1)
+            {
+                pictureBox1.Left = 0;
+                pictureBox2.Left = 0;
+                pictureBox3.Left = 0;
+                lblsonuc.Text = "";
+                bittimi = 0;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BirinciAtSolaUzaklik = pictureBox1.Left;
+            ikinciAtSolaUzaklik = pictureBox2.Left;
+            UcuncuAtSolaUzaklik = pictureBox2.Left;
+        }
+    }
+}
